6d23861c89cfa2f9d50c457f3ffbfd76f8350e66
michaelbag8 <michaelbag8@gmail.com>
2026-07-08 20:52:55 +0100

    go mod added

--- main.go
+++ main.go
@@ -2,33 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
+	"groupie_tracker/services"
 )
-
-func fetchArtists(url string) (string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", fmt.Errorf("error getting data from API %w", err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return "",fmt.Errorf("error getting data from API %d", resp.StatusCode)
-	}
-
-	content, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", fmt.Errorf("error reading body %w", err)
-	}
-
-	return string(content), nil
-}
 
 func main() {
 	url := "https://groupietrackers.herokuapp.com/api/artists"
 
-	res, err := fetchArtists(url)
+	res, err := services.FetchArtists(url)
 	if err != nil {
 		log.Fatal(err)
 	}
