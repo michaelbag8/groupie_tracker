1eaad5df4aa1366fa58ef371aafd0d916cc33846
michaelbag8 <michaelbag8@gmail.com>
2026-07-11 12:01:52 +0100

    utils package added

--- services/serve.go
+++ services/serve.go
@@ -73,25 +73,3 @@ func DecodeArtists(jsonStr string) ([]models.Artist, error) {
 	return result, nil
 }
 */	
-func BuildRelationsMap(items []models.RelationEntry) map[int]models.RelationEntry {
-    result := make(map[int]models.RelationEntry)
-    for _, item := range items {
-        result[item.ID] = item
-    }
-    return result
-}
-func BuildDatesMap(items []models.DateEntry) map[int]models.DateEntry {
-    result := make(map[int]models.DateEntry)
-    for _, item := range items {
-        result[item.ID] = item
-    }
-    return result
-}
-
-func BuildLocationsMap(items []models.LocationEntry) map[int]models.LocationEntry {
-    result := make(map[int]models.LocationEntry)
-    for _, item := range items {
-        result[item.ID] = item
-    }
-    return result
-}
---
+++ utils/utils.go
@@ -0,0 +1,27 @@
+package utils
+
+import(
+	"groupie_tracker/models"
+)
+func BuildRelationsMap(items []models.RelationEntry) map[int]models.RelationEntry {
+    result := make(map[int]models.RelationEntry)
+    for _, item := range items {
+        result[item.ID] = item
+    }
+    return result
+}
+func BuildDatesMap(items []models.DateEntry) map[int]models.DateEntry {
+    result := make(map[int]models.DateEntry)
+    for _, item := range items {
+        result[item.ID] = item
+    }
+    return result
+}
+
+func BuildLocationsMap(items []models.LocationEntry) map[int]models.LocationEntry {
+    result := make(map[int]models.LocationEntry)
+    for _, item := range items {
+        result[item.ID] = item
+    }
+    return result
+}
