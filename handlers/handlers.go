cb702c9c2a55528bfea1e7c9f228613009fbc547
michaelbag8 <michaelbag8@gmail.com>
2026-07-14 10:21:15 +0100

    templates filefixed to handle all cards

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,13 +11,13 @@ func MakeHomeHandler(fullArtists []models.FullArtist) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
 		temp, err := template.ParseFiles("templates/index.html")
 		if err !=nil{
-			http.Error(w, "error parsing files", http.StatusInternalServerError)
+			http.Error(w, "error parsing template file", http.StatusInternalServerError)
 			return 
 		}
 
 		err = temp.Execute(w, fullArtists)
 		if err !=nil{
-			http.Error(w, "error parsing files", http.StatusInternalServerError)
+			http.Error(w, "error executing template", http.StatusInternalServerError)
 			return 
 		}
 		
